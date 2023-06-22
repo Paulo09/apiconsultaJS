9bd960ecfc74090dd9f36901688aa77a930c4ca9
Paulo Castro <paullocasttro@hotmail.com>
2023-06-22 20:50:58 -0300

    Add files via upload

--- aulaAsync.js
+++ aulaAsync.js
@@ -1,10 +1,17 @@
-const construir_tabela = async () =>{
-   
-    const content = document.getElementById('content');
+
+const buscarusuarios = async () => {
 
     const response = await fetch('https://jsonplaceholder.typicode.com/users')
-
     const usuarios = await response.json();
+
+    return usuarios;
+
+}
+const construir_tabela = async () =>{
+   
+    const usuarios = await buscarusuarios();
+     
+    const content = document.getElementById('content');
 
     const usuariosHTML = usuarios.map( element =>
 
