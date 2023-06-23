88129397ec845b943d801c9b3d5a40cb274628c4
Paulo Castro <paullocasttro@hotmail.com>
2023-06-22 21:02:02 -0300

    Async

--- aulaAsync.js
+++ aulaAsync.js
@@ -7,25 +7,28 @@ const buscarusuarios = async () => {
     return usuarios;
 
 }
-const construir_tabela = async () =>{
+
+const usuarioToRowHTML = (usuario) =>{
+
+return `
+    <tr>
+        <td>${usuario.id}</td>
+        <td>${usuario.name}</td>
+        <td>${usuario.email}</td>
+        <td>${usuario.username}</td>
+        <td>${usuario.website}</td>
+    </tr>
+    `
+
+}
+const construir_tabela = async () => {
    
     const usuarios = await buscarusuarios();
      
     const content = document.getElementById('content');
 
-    const usuariosHTML = usuarios.map( element =>
-
-        `
-        <tr>
-            <td>${element.id}</td>
-            <td>${element.name}</td>
-            <td>${element.email}</td>
-            <td>${element.username}</td>
-            <td>${element.website}</td>
-        </tr>
-        `
-    )
-
+    const usuariosHTML = usuarios.map(usuarioToRowHTML)
+    
     usuariosHTML.forEach((elementoHTML) => content.innerHTML += elementoHTML)
 }
 
